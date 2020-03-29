0bf9de5fa5c2a2ca1e7e37d082a480fd4ef57390
Monica Gerard <mlgerardvla@gmail.com>
2020-03-29 00:20:28 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,32 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    reset!
+  end
+
+  def cells
+    @cells
+  end
+
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts(" #{cells[0]} | #{cells[1]} | #{cells[2]} ")
+    puts("-----------")
+    puts(" #{cells[3]} | #{cells[4]} | #{cells[5]} ")
+    puts("-----------")
+    puts(" #{cells[6]} | #{cells[7]} | #{cells[8]} ")
+  end
+
+  def position(cell_number)
+    @cells[cell_number.to_i - 1]
+  end
+
+  def full?
+    
+  end
+
+end
