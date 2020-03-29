1b2997a65aa32a3e8f5baa845d30560cb4a7c88b
Monica Gerard <mlgerardvla@gmail.com>
2020-03-29 06:00:48 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -8,7 +8,7 @@ class Game
                       [0, 4, 8], [6, 4, 2]]
 
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
-    binding.pry
+    # binding.pry
     @board = board
 
     @player_1 = player_1
@@ -16,7 +16,7 @@ class Game
   end
 
   def board
-    @board = []
+    @board
   end
 
   def player_1
@@ -28,8 +28,28 @@ class Game
   end
 
   def current_player
-
+    if @board.cells.select {|cell| cell != " "}.length.odd?
+      return player_2
+    else
+      return player_1
+    end
   end
 
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
 
+      position_1 = @board.cells[win_index_1]
+      position_2 = @board.cells[win_index_2]
+      position_3 = @board.cells[win_index_3]
+
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
+        (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return win_combination
+      end
+    end
+    false
+  end
 end
